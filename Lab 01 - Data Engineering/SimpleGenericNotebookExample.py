a7ef82fbaa57b7647bab15b4de6f71bc6057dfc8
jmsmcniff <jmsmcniff>
2022-12-24 01:24:11 +0000

    add csv delim for wine quality dataset

--- Lab 01 - Data Engineering/SimpleGenericNotebookExample.py
+++ Lab 01 - Data Engineering/SimpleGenericNotebookExample.py
@@ -13,19 +13,21 @@
 dbutils.widgets.text("database_name", "james_mcniff_parameter_demo")
 dbutils.widgets.text("dataset_name", "taxi_payment_type")
 dbutils.widgets.text("dataset_path", "/databricks-datasets/nyctaxi/taxizone/taxi_payment_type.csv")
+dbutils.widgets.text("csv_delimiter", ",")
 
 # COMMAND ----------
 
 database_name = dbutils.widgets.get("database_name")
 dataset_name = dbutils.widgets.get("dataset_name")
 dataset_path = dbutils.widgets.get("dataset_path")
-
+csv_delimiter = dbutils.widgets.get("csv_delimiter")
 
 # COMMAND ----------
 
 print(f"Database name: {database_name}")
 print(f"Dataset name: {dataset_name}")
 print(f"Dataset path: {dataset_path}")
+print(f"CSV Delimiter: {csv_delimiter}")
 
 # COMMAND ----------
 
@@ -39,6 +41,7 @@ spark.sql(f"USE {database_name};")
 # MAGIC   path = "${dataset_path}",
 # MAGIC   header = "true",
 # MAGIC   inferSchema = "true",
+# MAGIC   sep = "${csv_delimiter}",
 # MAGIC   quote = "\"",
 # MAGIC   mode = "FAILFAST" -- abort file parsing with a RuntimeException if any malformed lines are encountered
 # MAGIC );
